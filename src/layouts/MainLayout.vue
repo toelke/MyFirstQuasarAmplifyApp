68d91ef3168a9cc444236cdeef7248e3a6ecac34
Philipp Tölke <ptoelke@tecracer.de>
2021-03-17 17:17:40 +0100

    Remove version

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Photo App
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
